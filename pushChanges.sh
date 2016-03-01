44e2673e12aadbd27a43e77d86584bc4dbade6e1
cbdrx <boudreaux.christopher@gmail.com>
2016-03-01 00:20:08 -0600

    Improves the webserver update script

--- pushChanges.sh
+++ pushChanges.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-sudo rm -rf /var/www/*
-sudo cp -r ./* /var/www/
+while true; do
+    read -p "Push to the webserver? " yn
+    case $yn in
+        [Yy]* ) sudo rm -rf /var/www/* ; sudo cp -r ./* /var/www/; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
