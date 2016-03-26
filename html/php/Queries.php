1902e37df2841032d58e05d15e8af8bb179edefd
Christopher Boudreaux <Christopher Boudreaux>
2016-03-26 18:20:53 -0500

    Adds the code for the mysql connection

---
+++ html/php/Queries.php
@@ -0,0 +1,20 @@
+<?php
+    function ConnectToDB()
+    {
+        $servername = "localhost";
+        $username = "student";
+        $password = "student";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+        echo "Connected successfully";
+        return $conn;
+    }
+    
+    
+?>
