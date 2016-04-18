5606d740798473e91745d4351935c12221fae4b0
Christopher Boudreaux <Christopher Boudreaux>
2016-04-17 20:09:28 -0500

    Adds some queries to the queries file

--- html/php/Queries.php
+++ html/php/Queries.php
@@ -1,5 +1,4 @@
 <?php
-
     // ConnectToDB
     // Returns a mysqli connection object, or causes an error page if it fails
     function ConnectToDB()
@@ -7,9 +6,10 @@
         $servername = "localhost";
         $username = "root";
         $password = "student";
+        $dbname = "KachingDB";
 
         // Create connection
-        $conn = new mysqli($servername, $username, $password);
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
         // Check connection
         if ($conn->connect_error) {
@@ -32,6 +32,42 @@
         $conn->close();
         return false;
     }
+    
+    function IncomeTransactionsForUser($username)
+    {
+        $conn = ConnectToDB();
+        
+        $query = "select * from incomeTransaction where FK_user = " . $username . " order by date desc;";
+        
+        $results = $conn->query($query);
+        $conn->close();
+        return $results;
+    }
+    
+    function ExpenseTransactionForUser($username)
+    {
+        $conn = ConnectToDB();
+        $query = "select * from expenseTransaction where FK_user = " . $username . " order by date desc;";
+        
+        $results = $conn->query($query);
+        $conn->close();
+        return $results;
+    }
+    
+    function CategoryTableRowsForUser($username)
+    {
+        $conn = ConnectToDB();
+        $query = "select c.name, c.income, SUM(e.amount), c.goal" . 
+            "from 
+            "where FK_user = " . $username . " order by date desc;";
+        
+        $results = $conn->query($query);
+        $conn->close();
+        return $results;
+        
+    }
+    
+    
     
     
 ?>
