a55de53d6f9bc30700d49df5db3e9158d9f86a7b
cbdrx <boudreaux.christopher@gmail.com>
2016-04-17 20:45:12 -0500

    DataTables for expense not working on index

--- html/index.php
+++ html/index.php
@@ -54,16 +54,10 @@
             <div class="row">
                 <div class="col-sm-1"> </div>
                 <div class="col-sm-10 tableWrapper" id="allExpenses">
-                    <table id="allExpenseTable" class="table table-striped">  
+                    <table id="allExpensesTable" class="table table-striped">  
                     <thead>  
                     <tr>  
-                        /* For each header do this */ 
-                        <th> ID </th>
-                        <th> Amount </th>
-                        <th> Date </th>
-                        <th> Category </th>
-                        <th> Business </th>
-                        <th> Account Number </th>  
+                        /* For each header do this */ <th> /* Header */ </th>  
                     </tr>  
                     </thead>  
                     <tbody> 
